942f19ae825f9642594ea134fe32d5850478d468
Gary Lin <garyqwertyuiop@gmail.com>
2021-04-14 11:34:04 +0800

    css and basic render

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MeetingRoomsComponent } from './meeting-rooms/meeting-rooms.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeetingRoomsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/meeting-rooms/meeting-rooms.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-rooms',
+  templateUrl: './meeting-rooms.component.html',
+  styleUrls: ['./meeting-rooms.component.scss']
+})
+export class MeetingRoomsComponent implements OnInit {
+
+  dummyData: any[] = [
+    { id: 'm1', room: 'Tokyo', url: "https://picsum.photos/600/1000/?random=30" },
+    { id: 'm2', room: 'Dresden', url: "https://picsum.photos/600/1000/?random=31" },
+    { id: 'm3', room: 'Mosco', url: "https://picsum.photos/600/1000/?random=32" }
+  ]
+
+  show = true
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.dummyData);
+  }
+
+  showUI = () => {
+    this.show = !this.show
+  }
+
+}
